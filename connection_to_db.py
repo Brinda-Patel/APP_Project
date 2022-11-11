dcd5c47067b58fce7a35495fe8e05d8c1f3d9ee9
konark2010 <konarkshah2010@gmail.com>
2022-11-11 03:30:26 -0500

    Committing Json and Connection to data base file

---
+++ connection_to_db.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+
+class DBConnection:
+    __instance = None
+
+    def __init__(self) -> None:
+        if DBConnection.__instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            DBConnection.__instance = self
+
+    @staticmethod
+    def get_instance():
+        if DBConnection.__instance is None:
+            DBConnection()
+        return DBConnection.__instance
+
+    def get_connection(self):
+        try:
+            self.conn = sqlite3.connect('dataset4.db')
+            self.cursor = self.conn.cursor()
+            print("Connection to database is successful")
+        except sqlite3.Error as e:
+            print("Connection to database is not successful")
+            print(e)
+
+    def close_connection(self):
+        try:
+            self.conn.close()
+            print("Connection to database is closed")
+        except sqlite3.Error as e:
+            print("Connection to database is not closed")
+            print(e)
+
+    def execute_query(self, query):
+        try:
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            self.conn.commit()
+            print("Query executed successfully")
+            return result
+
+        except sqlite3.Error as e:
+            print("Query is not executed")
+            print(e)
+            
