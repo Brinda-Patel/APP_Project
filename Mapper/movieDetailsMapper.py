76107fd76bd70c24194e6138bfe7d2eabb0f9f39
Brinda Patel <brindapatel21625@gmail.com>
2022-11-13 02:26:59 -0500

    Create and insert query

--- Mapper/movieDetailsMapper.py
+++ Mapper/movieDetailsMapper.py
@@ -7,9 +7,24 @@ class MovieDetailsMapper:
         self.conn.get_connection()
 
     def insert_moviedetails(self, movieDetails):
-        query = f'INSERT INTO (title,movieIMDbRating,totalRatingCount,totalUserReviews,totalCriticReviews,director_id,datePublished,leadActor_id,description) VALUES ("{movieDetails.get_title()}", "{movieDetails.get_movieIMDbRating()}", "{movieDetails.get_totalRatingCount()}", "{movieDetails.get_totalUserReviews()}", "{movieDetails.get_totalCriticReviews()}", "{movieDetails.get_director_id()}", "{movieDetails.get_datePublished()}", "{movieDetails.get_leadActor_id()}", "{movieDetails.get_description()}", "{movieDetails.get_duration()}")'
-        return self.conn.execute_query(query)
+        query = f'INSERT INTO movieDetail(title,movieIMDbRating,totalRatingCount,totalUserReviews,totalCriticReviews,datePublished,director_id,leadActor_id,description,duration) VALUES ("{movieDetails.get_title()}", "{movieDetails.get_movieIMDbRating()}", "{movieDetails.get_totalRatingCount()}", "{movieDetails.get_totalUserReviews()}", "{movieDetails.get_totalCriticReviews()}", "{movieDetails.get_datePublished()}","{movieDetails.get_director_id()}", "{movieDetails.get_leadActor_id()}", "{movieDetails.get_description()}", "{movieDetails.get_duration()}") ON CONFLICT (title) DO NOTHING'
+        self.conn.execute_query(query)
 
     def create_moviedetails(self):
-        query = f'CREATE TABLE if NOT EXISTS movieDetail (movieDetail_id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR (20), movieIMDbRating  DECIMAL (2, 1), totalRatingCount INTEGER, totalUserReviews VARCHAR (20), totalCriticReviews VARCHAR (20), director_id INTEGER REFERENCES director (director_id), datePublished DATE, leadActor_id INTEGER REFERENCES leadActor (leadActor_id), description TEXT, duration INTEGER)'
+        query = f'CREATE TABLE if NOT EXISTS movieDetail (movieDetail_id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR (20) UNIQUE, movieIMDbRating  DECIMAL (2, 1), totalRatingCount INTEGER, totalUserReviews VARCHAR (20), totalCriticReviews VARCHAR (20), director_id INTEGER REFERENCES director (director_id), datePublished DATE, leadActor_id INTEGER REFERENCES leadActor (leadActor_id), description TEXT, duration INTEGER)'
+        print("Table Created Successfully")
         return self.conn.execute_query(query)
+
+    # def select(self, dictionary={}):
+    #     query = f'SELECT * FROM movieDetail WHERE '
+    #     for key in dictionary:
+    #         query += f'{key} = "{dictionary[key]}" AND '
+    #     query = query[:-5]
+    #     return self.conn.execute_query(query)
+
+    def select(self):
+        query = f'select * from movieDetail where movieIMDbRating > 8.5'
+        result = self.conn.execute_query(query)
+        print(result)
+        # for data in result:
+        #     print(data)
